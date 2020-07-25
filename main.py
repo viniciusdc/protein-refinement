dfcc84e39c10ac28b1861479ae669b0a95981b4a
viniciusdc <vinivdc2009@hotmail.com>
2020-07-24 21:07:43 -0300

    created start information

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-# check config. information
-# execute tests
-# check viability status
-# execute (run or reload)
+import logging
+
+
+def main() -> None:
+    """This code verifies the config file information and runs the tests for structural viability"""
+    # check for config.yaml file
+    # run environment tests
+    # run structure tests
+    # run solvers tests: both phases
+    # if all done execute run.py; else check missing information and reload
+    return
+
+
+if __name__ == '__main__':
+    main()
